fd63ee085c60b1ccefd55953f27c1e89f9ddc7c6
Torrex1 <flendik666@gmail.com>
2024-07-31 22:54:37 +0300

    radio btn upd

--- src/App.vue
+++ src/App.vue
@@ -9,36 +9,43 @@
     <ul class="radioButtonList">
       <li>
         <input id="rb1" type="radio">
+        <span class="customRadio"></span>
         <label for="rb1">Текст переключателя</label>
       </li>
 
       <li>
         <input id="rb2" type="radio">
+        <span class="customRadio"></span>
         <label for="rb2">Текст переключателя</label>
       </li>
 
       <li>
         <input id="rb3" type="radio">
+        <span class="customRadio"></span>
         <label for="rb3">Текст переключателя</label>
       </li>
 
       <li>
         <input id="rb4" type="radio">
+        <span class="customRadio"></span>
         <label for="rb4">Текст переключателя</label>
       </li>
 
       <li>
         <input id="rb5" type="radio">
+        <span class="customRadio"></span>
         <label for="rb5">Текст переключателя</label>
       </li>
 
       <li>
         <input id="rb6" type="radio">
+        <span class="customRadio"></span>
         <label for="rb6">Текст переключателя</label>
       </li>
 
       <li>
         <input id="rb7" type="radio">
+        <span class="customRadio"></span>
         <label for="rb7">Текст переключателя</label>
       </li>
 
@@ -72,15 +79,48 @@ label {
   font-size: 18px;
   display: flex;
   align-items: center;
-  gap: 10px;
+  margin-left: 10px;
 }
 
+.customRadio {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  border: 1px solid var(--border-color);
+  background-color: #fff;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+input[type="radio"]:checked + .customRadio::before {
+  content: "";
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background-color: var(--radio-bg);
+  border-radius: 50%;
+  margin: 5px;
+}
+
+/* 
 label::before {
   content: "";
   display: inline-block;
   width: 20px;
   height: 20px;
   border: 1px solid var(--border-color);
+  background-color: #fff;
   border-radius: 50%;
 }
+
+input[type="radio"]:checked + label::before {
+  background-color: var(--radio-bg);
+} */
+
+
+
+/* input[type="radio"]:checked + label::before {
+  background-color: var(--radio-bg);
+} */
+
 </style>
