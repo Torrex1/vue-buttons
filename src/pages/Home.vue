8772ac8441cdd68ff53413d6ed8c5d85704cf361
Torrex1 <flendik666@gmail.com>
2024-08-03 02:27:45 +0300

    add feedback

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -26,11 +26,26 @@
             </div>
         </div>
     </main>
+
+    <footer>
+        <label>Оставить
+            <a href="mailto:TarikulievRM@stud.rosnou.ru?cc=flendik666@gmail.com&subject=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%2C%20%D0%B4%D0%BE%D1%80%D0%BE%D0%B3%D0%BE%D0%B9%20%D0%BF%D1%80%D0%BE%D0%B2%D0%B5%D1%80%D1%8F%D1%8E%D1%89%D0%B8%D0%B9%21%20%D0%9E%D1%86%D0%B5%D0%BD%D0%B8%20%D0%BA%D0%B0%D1%87%D0%B5%D1%81%D1%82%D0%B2%D0%BE%20%D0%BF%D1%80%D0%BE%D0%B4%D0%B5%D0%BB%D0%B0%D0%BD%D0%BD%D0%BE%D0%B9%20%D0%BC%D0%BD%D0%BE%D0%B9%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%8B%20%D0%BF%D0%B6%D1%88%D0%BA%D0%B0%20%3A%29&body=%D0%A7%D0%B5%D0%BA%D0%B1%D0%BE%D0%BA%D1%81%D1%8B%20%D0%B8%20%D1%80%D0%B0%D0%B4%D0%B8%D0%BE%20%D0%BA%D0%BD%D0%BE%D0%BF%D0%BA%D0%B8%3A%20___%20%28%D0%BE%D1%82%201%20%D0%B4%D0%BE%205%29%20%0A%D0%97%D0%B0%D0%B3%D1%80%D1%83%D0%B7%D1%87%D0%B8%D0%BA%20%D1%84%D0%B0%D0%B9%D0%BB%D0%BE%D0%B2%3A%20___%20%28%D0%BE%D1%82%201%20%D0%B4%D0%BE%205%29%0A%D0%9A%D0%BE%D0%BC%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D1%80%D0%B8%D0%B8%2C%20%D0%BF%D0%BE%D0%B6%D0%B5%D0%BB%D0%B0%D0%BD%D0%B8%D1%8F%20%D0%BF%D0%BE%20%D1%83%D0%BB%D1%83%D1%87%D1%88%D0%B5%D0%BD%D0%B8%D1%8E%3A">
+                feedback ☺
+            </a>
+        </label>
+    </footer>
     
 </template>
 
 <style scoped>
 /* стилизация кнопок на странице */
+footer label {
+    font-size: 23px;
+    position: absolute;
+    bottom: 20px;
+    right: 50px;
+}
+
 button {
     padding: 20px;
     width: 300px;
