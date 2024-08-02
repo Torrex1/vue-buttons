aa9a1c6924722b7d87f7e230f7522d2b7d8b4502
Torrex1 <flendik666@gmail.com>
2024-08-03 01:29:57 +0300

    minor edit

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <script setup>
-import RadioBtn from './components/RadioBtn.vue';
-import Checkboxes from './components/Checkboxes.vue';
 </script>
 
 <template>
-    <div style="display: grid; grid-template-columns: 1fr 1fr">
-        <RadioBtn />
-        <Checkboxes />
-    </div>
-    
+    <router-view></router-view>
 </template>
 
 <style scoped>
