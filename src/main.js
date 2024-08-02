44cfcfc7bebad7e2975004d906697e68ce6526ac
Torrex1 <flendik666@gmail.com>
2024-08-03 01:28:47 +0300

    init vue router for fun :)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Home from './pages/Home.vue'
+import Second from './pages/Second.vue'
+import Buttons from './pages/Buttons.vue'
+
+const app = createApp(App)
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', name: 'Home', component: Home},
+        {path: '/second', name: 'Second', component: Second},
+        {path: '/buttons', name: 'Buttons', component: Buttons},
+    ]
+})
+
+app.use(router)
+
+app.mount('#app')
+
