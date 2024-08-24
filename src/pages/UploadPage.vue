827df14a120faa455f1584ba4a32ecc1e2bbe5b1
Torrex1 <flendik666@gmail.com>
2024-08-24 13:34:14 +0300

    page name upd

--- src/pages/UploadPage.vue
+++ src/pages/UploadPage.vue
@@ -1,11 +1,13 @@
 <script setup>
     import UploadFile from '@/components/UploadFile.vue'; 
+    import Loader from '@/components/Loader.vue';
 </script>
 
 <template>
     <div class="wrapper">
         <h1>Upload file</h1>
         <UploadFile />
+        <Loader />
     </div>
     
 </template>
