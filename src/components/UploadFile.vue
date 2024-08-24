826b9c30a78aac7082898c92a61624be3c5fa5dc
Torrex1 <flendik666@gmail.com>
2024-08-24 04:02:45 +0300

    added error state

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -1,10 +1,34 @@
+<script setup>
+    import { ref } from 'vue';
+
+    const hasError = ref(false);
+
+    const onFileChange = () => {
+        const file = event.target.files[0];
+
+        // files.value = [...files.value, file];
+        checkingFileType(file)
+    }
+
+    const checkingFileType = (file) => {
+        if (file.type === 'image/jpeg' || file.type === 'image/png' 
+            || file.type === 'application/pdf') {
+                hasError.value = false;
+        } else {
+            hasError.value = true;
+        }
+    }
+
+</script>
+
 <template>
     <label>
-        <input type="file" accept=".png, .jpg, .pdf">
+        <input @change="onFileChange" type="file" accept=".png, .jpg, .pdf">
 
-        <div class="upload-file-wrapper">
+        <div :class="{uploadFileWrapper: !hasError, wrapperError: hasError }">
             <div class="upload-file-box">
-                <img style="width: 100px; height: 100px;" src="../assets/img/upload-file-icon.png" alt="uploadIcon">
+                <img v-if="!hasError" style="width: 100px; height: 100px;" src="../assets/img/upload-file-icon.png" alt="uploadIcon">
+                <img v-else style="width: 100px; height: 100px;" src="../assets/img/upload-file-error.png" alt="uploadIcon">
                 <span>Перетащите файлы сюда или кликните на область</span>
                 <span>Принимаются файлы в формате PDF, JPG, PNG. Пожалуйста, убедитесь, что текст на предоставленных копиях разборчив.</span>
             </div>
@@ -14,7 +38,7 @@
 </template>
 
 <style scoped>
-.upload-file-wrapper {
+.uploadFileWrapper {
     width: 656px;
     height: 224px;
     display: flex;
@@ -29,7 +53,7 @@
     font-size: 16px;
 }
 
-.upload-file-wrapper:hover {
+.uploadFileWrapper:hover {
     background-color: #F0F5FB;
 }
 
@@ -54,5 +78,21 @@ label {cursor:pointer;}
 input {
     position: absolute;
     visibility: hidden;
+}
+
+.wrapperError {
+    width: 656px;
+    height: 224px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    padding: 32px 32px 40px 32px;
+    border: 2px dashed var(--error-border);
+    background-color: var(--error-bg);
+    border-radius: 8px;
+    color: var(--error-border);
+    font-size: 16px;
 }
 </style>
