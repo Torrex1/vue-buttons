d4022224221acfa739e960608750e91a4c965786
Torrex1 <flendik666@gmail.com>
2024-09-23 01:19:36 +0300

    upd

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,6 @@
 <template>
     <header class="container">
-        <h2>Привет, это что-то типо лобби. 
-            Отсюда ты можешь попасть куда захочешь. 
-            Жмакай по кнопкам, они приведут тебя на нужное задание :D 
-        </h2>
+        <h2>Привет, тык по кнопкам для перехода на нужную страницу</h2>
     </header>
 
     <main>
@@ -13,7 +10,7 @@
                 <h3>Radio & Checkboxes</h3>
             
                 <RouterLink to="/buttons">
-                    <button>вперед к кнопкам</button>
+                    <button>Кастомные кнопочки</button>
                 </RouterLink>
             </div>
 
@@ -21,20 +18,11 @@
                 <h3>Upload file task</h3>
 
                 <RouterLink to="/upload-file">
-                    <button>1...2..3... Поехали</button>
+                    <button>Кастомный загрузчик файлов</button>
                 </RouterLink>
             </div>
         </div>
     </main>
-
-    <footer>
-        <label>Оставить
-            <a href="mailto:TarikulievRM@stud.rosnou.ru?cc=flendik666@gmail.com&subject=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%2C%20%D0%B4%D0%BE%D1%80%D0%BE%D0%B3%D0%BE%D0%B9%20%D0%BF%D1%80%D0%BE%D0%B2%D0%B5%D1%80%D1%8F%D1%8E%D1%89%D0%B8%D0%B9%21%20%D0%9E%D1%86%D0%B5%D0%BD%D0%B8%20%D0%BA%D0%B0%D1%87%D0%B5%D1%81%D1%82%D0%B2%D0%BE%20%D0%BF%D1%80%D0%BE%D0%B4%D0%B5%D0%BB%D0%B0%D0%BD%D0%BD%D0%BE%D0%B9%20%D0%BC%D0%BD%D0%BE%D0%B9%20%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D1%8B%20%D0%BF%D0%B6%D1%88%D0%BA%D0%B0%20%3A%29&body=%D0%A7%D0%B5%D0%BA%D0%B1%D0%BE%D0%BA%D1%81%D1%8B%20%D0%B8%20%D1%80%D0%B0%D0%B4%D0%B8%D0%BE%20%D0%BA%D0%BD%D0%BE%D0%BF%D0%BA%D0%B8%3A%20___%20%28%D0%BE%D1%82%201%20%D0%B4%D0%BE%205%29%20%0A%D0%97%D0%B0%D0%B3%D1%80%D1%83%D0%B7%D1%87%D0%B8%D0%BA%20%D1%84%D0%B0%D0%B9%D0%BB%D0%BE%D0%B2%3A%20___%20%28%D0%BE%D1%82%201%20%D0%B4%D0%BE%205%29%0A%D0%9A%D0%BE%D0%BC%D0%BC%D0%B5%D0%BD%D1%82%D0%B0%D1%80%D0%B8%D0%B8%2C%20%D0%BF%D0%BE%D0%B6%D0%B5%D0%BB%D0%B0%D0%BD%D0%B8%D1%8F%20%D0%BF%D0%BE%20%D1%83%D0%BB%D1%83%D1%87%D1%88%D0%B5%D0%BD%D0%B8%D1%8E%3A">
-                feedback ☺
-            </a>
-        </label>
-    </footer>
-    
 </template>
 
 <style scoped>
